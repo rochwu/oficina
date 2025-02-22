869c0d2eac206ce60f399ceb7897cef43be508f8
Rolando Wu <rolando.che.wu@gmail.com>
2025-02-22 04:09:46 -0800

    Forgot the main file

---
+++ src/store/today.ts
@@ -0,0 +1,28 @@
+import {createSignal} from 'solid-js';
+
+export const [today, setToday] = createSignal(new Date());
+
+document.addEventListener('visibilitychange', () => {
+  if (document.visibilityState !== 'visible') {
+    return;
+  }
+
+  const oldDate = today();
+  const newDate = new Date();
+
+  // I even benchmarked ways to compare dates, this is fastest
+  if (
+    // If we pass to another day, change it
+    !(
+      // We're so extra that we event optimized the order
+      // And we've also benchmarked AND vs OR, AND was faster, by a wee bit
+      (
+        oldDate.getDate() === newDate.getDate() &&
+        oldDate.getMonth() === newDate.getMonth() &&
+        oldDate.getFullYear() === newDate.getFullYear()
+      )
+    )
+  ) {
+    setToday(newDate);
+  }
+});
