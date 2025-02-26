e7fb18f74eea4bf67fcf352f45707e613869cefb
Rolando Wu <rolando.che.wu@gmail.com>
2025-02-26 02:08:29 -0800

    Fixed irresponsive intersection observer

--- src/Month/Month.tsx
+++ src/Month/Month.tsx
@@ -7,13 +7,7 @@ import { MonthProvider, useQuarter, YearProvider } from '../Context';
 import { vars } from '../css';
 import { getDays, getFirstDayOfWeek } from '../date';
 import { Day } from '../Day';
-import {
-  setGridIndex,
-  setVisibleQ,
-  today,
-  gridIndex,
-  setIsScrolling,
-} from '../store';
+import { setGridIndex, setVisibleQ, today, gridIndex } from '../store';
 import type { Quarter, Ym } from '../types';
 
 const Container = styled.div({
@@ -62,7 +56,7 @@ export const Month = (props: Props) => {
 
     createEffect(() => {
       if (gridIndex() === local.index) {
-        setIsScrolling(true);
+        // setIsScrolling(true);
         ref.scrollIntoView({ behavior: 'smooth' });
 
         // Update later, we probs want to do it after we've arrived at the month
