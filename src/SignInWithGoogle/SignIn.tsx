f33088dad822b96d6a93f7d785e6f2b94321a4fc
Rolando Wu <rolando.che.wu@gmail.com>
2025-02-25 21:43:52 -0800

    Chunked Google

---
+++ src/SignInWithGoogle/SignIn.tsx
@@ -0,0 +1,32 @@
+import './google.css';
+
+import { getAuth, GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
+import { styled } from 'solid-styled-components';
+
+import { vars } from '../css';
+import html from './google.html?raw';
+import { app } from '../firebase/app';
+
+const Position = styled.div({
+  position: 'absolute',
+  top: vars.gap,
+  right: vars.gap,
+  padding: vars.gap, // This luckily makes it fit at the select
+});
+
+/**
+ * I split this so that it can be chunked
+ *
+ * Get the button html and css https://developers.google.com/identity/branding-guidelines
+ */
+export const SignIn = () => {
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const signIn = () => {
+    signInWithRedirect(auth, provider);
+  };
+
+  // eslint-disable-next-line solid/no-innerhtml
+  return <Position innerHTML={html} onClick={signIn} />;
+};
--- src/SignInWithGoogle/SignInWithGoogle.tsx
+++ src/SignInWithGoogle/SignInWithGoogle.tsx
@@ -1,29 +1,14 @@
-import './google.css';
+import { getAuth, getRedirectResult, onAuthStateChanged } from 'firebase/auth';
+import { createResource, lazy, Show } from 'solid-js';
 
-import {
-  getAuth,
-  getRedirectResult,
-  GoogleAuthProvider,
-  onAuthStateChanged,
-  signInWithRedirect,
-} from 'firebase/auth';
-import { createResource, Show } from 'solid-js';
-import { styled } from 'solid-styled-components';
-
-import { vars } from '../css';
 import { app } from '../firebase/app';
 import { setUser, user } from '../store';
-import html from './google.html?raw';
 
-const Position = styled.div({
-  position: 'absolute',
-  top: vars.gap,
-  right: vars.gap,
-  padding: vars.gap, // This luckily makes it fit at the select
-});
+const SignIn = lazy(async () => ({
+  default: (await import('./SignIn')).SignIn,
+}));
 
 const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
 
 // Listen for changes in authentication state
 onAuthStateChanged(auth, (user) => {
@@ -49,18 +34,11 @@ const [isNotRedirect] = createResource(() => getRedirectResult(auth));
  * Cloudflare to redirect `rolando.ooo/oficina/__/auth/handler` to `dame-follow.firebaseapp.com/__/auth/handler`
  * Cloud Console OAuth to have `rolando.ooo/oficina/__/auth/handler` as an authorized redirect URL
  * Firebase Hosting to host the auth files, this is auto available by Firebase
- *
- * Get the button html and css https://developers.google.com/identity/branding-guidelines
  */
 export const SignInWithGoogle = () => {
-  const signIn = () => {
-    signInWithRedirect(auth, provider);
-  };
-
   return (
     <Show when={isNotRedirect.state === 'ready' && !user()}>
-      {/* eslint-disable-next-line solid/no-innerhtml */}
-      <Position innerHTML={html} onClick={signIn} />
+      <SignIn />
     </Show>
   );
 };
