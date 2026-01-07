384ead23aa0cc3d2a07fd2e129fbac2a27b7d8bb
Rolando Wu <rolando.che.wu@gmail.com>
2026-01-06 21:33:17 -0800

    Fix new year problem and date cycle

--- src/date/quarters.ts
+++ src/date/quarters.ts
@@ -31,12 +31,32 @@ export const getQuarters = ({ month, year }: Ym) => {
   return {
     last: {
       quarter: lastQ,
-      yms: getYms({ year: nowQ === 0 ? year - 1 : year, quarter: lastQ }),
+      yms: getYms({
+        /**
+         * Q0 ie: Feb, Mar, Apr; previous Q3 has Nov, Dec in the previous year
+         * Jan in Q3 has previous Q2 in the previous year
+         */
+        year: nowQ === 0 || month === 0 ? year - 1 : year,
+        quarter: lastQ,
+      }),
     },
-    now: { quarter: nowQ, yms: getYms({ year, quarter: nowQ }) },
+    now: {
+      quarter: nowQ,
+      /**
+       * getYms +1 the year for Q3's Jan, so we minus 1 to normalize
+       */
+      yms: getYms({ year: month === 0 ? year - 1 : year, quarter: nowQ }),
+    },
     next: {
       quarter: nextQ,
-      yms: getYms({ year: nowQ === 3 ? year + 1 : year, quarter: nextQ }),
+      yms: getYms({
+        /**
+         * Q3 ie: Nov, Dec, Jan; Nov and Dec have next Q0 in the next year
+         * Jan in Q3 is the same year as the next
+         */
+        year: nowQ === 3 && month !== 0 ? year + 1 : year,
+        quarter: nextQ,
+      }),
     },
   };
 };
